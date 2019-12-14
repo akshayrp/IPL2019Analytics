c4037174de1384e0fa1dd11b328107cc872f5c7c
Akshay <akshaypatwari24@gmail.com>
2019-12-14 16:41:06 +0530

    Refactor : used Java 8 Iterable and Stream to iterate and get count

--- src/main/java/iplAnalyzer/IPLAnalyzer.java
+++ src/main/java/iplAnalyzer/IPLAnalyzer.java
@@ -7,30 +7,31 @@ import java.io.IOException;
 import java.io.Reader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.stream.StreamSupport;
 
 public class IPLAnalyzer
 {
+   Map<String,RunsCsvBinder> runsMap = new HashMap<>();
    public int loadData(String iplFilePath) throws IPLException
    {
-      int playersCount=0;
-      try( Reader reader = Files.newBufferedReader(Paths.get(iplFilePath)))
+      int playersCount = 0;
+      try (Reader reader = Files.newBufferedReader(Paths.get(iplFilePath)))
       {
          ICSVBuilder csvBuilder = CSVBuilderFactory.createCSVBuilder();
          Iterator csvFileIterator = csvBuilder.getCSVFileIterator(reader, RunsCsvBinder.class);
-         while(csvFileIterator.hasNext())
-         {
-            playersCount++;
-            csvFileIterator.next();
-         }
+         Iterable csvIterable = () -> csvFileIterator;
+         playersCount = (int) StreamSupport.stream(csvIterable.spliterator(),false).count();
       }
       catch (IOException e)
       {
-         throw new IPLException("Error in File Reading",IPLException.ExceptionType.CANNOT_READ_FILE);
+         throw new IPLException("Error in File Reading", IPLException.ExceptionType.CANNOT_READ_FILE);
       }
       catch (CSVBuilderException e)
       {
-         throw new IPLException("Unable to Build Bean",IPLException.ExceptionType.UNABLE_TO_PARSE);
+         throw new IPLException("Unable to Build Bean", IPLException.ExceptionType.UNABLE_TO_PARSE);
       }
       return playersCount;
    }
